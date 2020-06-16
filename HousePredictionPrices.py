115321be35fdf5dd0652c49cc36d64d8854e37e6
elliemehl <51216830+elliemehl@users.noreply.github.com>
2020-06-16 12:06:37 -0400

    Update HousePredictionPrices.py

--- HousePredictionPrices.py
+++ HousePredictionPrices.py
@@ -3,13 +3,28 @@ import pandas as pd
 import numpy as np
 import sklearn as sk
 
+from sklearn.linear_model import LinearRegression
+
 #Opening data
-raw_house_data = pd.read_csv("/Users/ellie/combinedhouse.csv", sep = ',')
+raw_house_data = pd.read_csv("/Users/ellie/combinedhouse.csv", sep=',')
 print("Shape of data: ", raw_house_data.shape)
 raw_house_data.head()
 
 #One hot encoding for categorical variables
-dummie_house_data = pd.get_dummies(data=raw_house_data, columns = ['MSZoning', 'Street', 'Alley', 'LotShape', 'LandContour', 'Utilities', 'LotConfig', 'LandSlope', 'Neighborhood', 'Condition1', 'Condition2', 'BldgType', 'HouseStyle', 'RoofStyle', 'RoofMatl', 'Exterior1st', 'Exterior2nd', 'MasVnrType', 'ExterQual', 'ExterCond', 'Foundation', 'BsmtQual', 'BsmtCond', 'BsmtExposure', 'BsmtFinType1', 'BsmtFinType2', 'Heating', 'HeatingQC', 'CentralAir', 'Electrical', 'KitchenQual', 'Functional', 'FireplaceQu', 'GarageType', 'GarageFinish', 'GarageQual', 'GarageCond', 'PavedDrive', 'PoolQC', 'Fence', 'MiscFeature', 'SaleType', 'SaleCondition'], drop_first=True)
+dummie_house_data = pd.get_dummies(data=raw_house_data, columns = ['MSZoning', 'Street', 'Alley', 'LotShape', 
+                                                                   'LandContour', 'Utilities', 'LotConfig', 
+                                                                   'LandSlope', 'Neighborhood', 'Condition1', 
+                                                                   'Condition2', 'BldgType', 'HouseStyle', 
+                                                                   'RoofStyle', 'RoofMatl', 'Exterior1st', 
+                                                                   'Exterior2nd', 'MasVnrType', 'ExterQual', 
+                                                                   'ExterCond', 'Foundation', 'BsmtQual', 
+                                                                   'BsmtCond', 'BsmtExposure', 'BsmtFinType1', 
+                                                                   'BsmtFinType2', 'Heating', 'HeatingQC', 
+                                                                   'CentralAir', 'Electrical', 'KitchenQual', 
+                                                                   'Functional', 'FireplaceQu', 'GarageType', 
+                                                                   'GarageFinish', 'GarageQual', 'GarageCond', 
+                                                                   'PavedDrive', 'PoolQC', 'Fence', 'MiscFeature',
+                                                                   'SaleType', 'SaleCondition'], drop_first=True)
 print("Shape of data: ", dummie_house_data.shape)
 dummie_house_data.head()
 
@@ -33,14 +48,13 @@ X_house.head()
 
 #Impoting the linear regression model
 #Fitting it to the training data
-from sklearn.linear_model import LinearRegression
-osl_sk = LinearRegression().fit(X_house, y_house)
+lr_sk = LinearRegression().fit(X_house, y_house)
 
 print("Intercept =", osl_sk.intercept_)
 ##print("Model coefficients =", osl_sk.coef_)
-print("R^2 =", osl_sk.score(X_house, y_house))
+print("R^2 =", lr_sk.score(X_house, y_house))
 
 #Predicting the sale prices of the testing data
 #Creating a csv file with the predictions to submit to kaggle
-y_predict_house = osl_sk.predict(X_predict_house)
+y_predict_house = lr_sk.predict(X_predict_house)
 np.savetxt("HousePredictionPrices4.csv", y_predict_house, delimiter=',')

275c712359b46468eafab3bc3dd53fd47bc75916
elliemehl <51216830+elliemehl@users.noreply.github.com>
2020-06-18 15:37:45 -0400

    Update TitanicSurvivalPredictions.py

--- TitanicSurvivalPredictions.py
+++ TitanicSurvivalPredictions.py
@@ -5,40 +5,34 @@ from sklearn.preprocessing import LabelEncoder
 from sklearn.tree import DecisionTreeClassifier
 
 #Loading the data
-#Dropping features 
+#Dropping features
 TITANIC_RAW_DATA = pd.read_csv("/Users/ellie/combinedtitanic.csv", sep=',')
-TITANIC_DROPPED_DATA = TITANIC_RAW_DATA.drop(['PassengerId', 'Name', 'SibSp',
-                                              'Parch', 'Ticket', 'Cabin',
-                                              'Embarked'], axis=1)
+TITANIC_RAW_DATA['Age'] = TITANIC_RAW_DATA['Age'].fillna(TITANIC_RAW_DATA['Age'].mean())
+TITANIC_RAW_DATA['Fare'] = TITANIC_RAW_DATA['Fare'].fillna(TITANIC_RAW_DATA['Fare'].median())
+TITANIC_RAW_DATA['Embarked'] = TITANIC_RAW_DATA['Embarked'].fillna('S')
+
+TITANIC_DROPPED_DATA = TITANIC_RAW_DATA.drop(['PassengerId', 'SibSp', 'Name',
+                                              'Parch', 'Ticket', 'Cabin'], axis=1)
 
 #Splitting data into training and testing
 N_TRAIN = 891
 TITANIC_DATA = TITANIC_DROPPED_DATA[:N_TRAIN]
 TITANIC_PREDICT = TITANIC_DROPPED_DATA[N_TRAIN:]
-TITANIC_DATA.tail()
 
 #Assigning target to be predicted and features of training data
-#Replace age blanks with the mean age
 Y_TITANIC = TITANIC_DATA['Survived']
 INPUTS_TITANIC = TITANIC_DATA.drop(['Survived'], axis=1)
-INPUTS_TITANIC['Age'] = INPUTS_TITANIC['Age'].fillna(INPUTS_TITANIC['Age'].mean())
 
-#Assigning 0 and 1 to sexes
-LE_SEX = LabelEncoder()
-INPUTS_TITANIC['Sex_n'] = LE_SEX.fit_transform(INPUTS_TITANIC['Sex'])
-X_TITANIC = INPUTS_TITANIC.drop(['Sex'], axis=1)
-X_TITANIC.tail()
+DUMMIES_TITANIC = pd.get_dummies(data=INPUTS_TITANIC, columns=['Embarked'], drop_first=True)
+LE_FEATURES = LabelEncoder()
+DUMMIES_TITANIC['Sex_n'] = LE_FEATURES.fit_transform(DUMMIES_TITANIC['Sex'])
+X_TITANIC = DUMMIES_TITANIC.drop(['Sex'], axis=1)
 
 #Assigning target to be predicted and features of testing data
-#Replacing the blank ages and fares with mean values
-X_TITANIC_PREDICT = TITANIC_PREDICT.drop(['Survived'], axis=1)
-TITANIC_PREDICT['Age'] = TITANIC_PREDICT['Age'].fillna(TITANIC_PREDICT['Age'].mean())
-TITANIC_PREDICT['Fare'] = TITANIC_PREDICT['Fare'].fillna(TITANIC_PREDICT['Fare'].mean())
-
-#Assigning 0 and 1 to sexes
-LE_SEX_PREDICT = LabelEncoder()
-TITANIC_PREDICT['Sex_n'] = LE_SEX_PREDICT.fit_transform(TITANIC_PREDICT['Sex'])
-X_PREDICT = TITANIC_PREDICT.drop(['Sex', 'Survived'], axis=1)
+DUMMIES_PREDICT = pd.get_dummies(data=TITANIC_PREDICT, columns=['Embarked'], drop_first=True)
+LE_FEATURES_PREDICT = LabelEncoder()
+DUMMIES_PREDICT['Sex_n'] = LE_FEATURES_PREDICT.fit_transform(DUMMIES_PREDICT['Sex'])
+X_PREDICT = DUMMIES_PREDICT.drop(['Sex', 'Survived'], axis=1)
 
 #Fitting the model
 MODEL = DecisionTreeClassifier()
@@ -54,4 +48,4 @@ SUBMISSION = pd.DataFrame({
         "Survived":Y_PREDICT_DT
     })
 
-SUBMISSION.to_csv('titanic_survival.csv', index=False)
+SUBMISSION.to_csv('titanic_survival3.csv', index=False)
